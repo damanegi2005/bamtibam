3c90ed8de0ece487e1aae66fca81d13a49dbfd4d
damanegi2005 <2467013@ewha.ac.kr>
2025-11-19 21:26:27 +0900

    api.js변경

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = window.API_BASE_URL || "https://bamtibam-server.onrender.com";
+const API_BASE_URL = "https://bamtibam-server.onrender.com";
 
 async function request(path, { method = 'GET', body, token } = {}) {
   const headers = { 'Content-Type': 'application/json' };
